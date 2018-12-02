bf3a1c2b766fc0fed14d95aa6535876ac8474c56
eduardoteodorus <eduardo.teodorus@gmail.com>
2018-12-02 13:38:02 -0200

    comment component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Post from './Post';
 
 export default class App extends React.Component {
 
     render() {
         return (
-            <h1>Hello World.</h1>
+            <div>
+                <Post title="Fazer café" />
+                <Post title="Estudar ReactJS"/>
+            </div>
         )
     }
 
---
+++ src/components/Post.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Comment from './Comment';
+
+export default class Post extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            comments: [],
+            newCommentText: '',
+        };
+
+        // informa ao react que quando utilizarmos this dentro do método handlesubmit
+        // estaremos referenciando a classe Post
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+
+        this.setState({
+           comments: [...this.state.comments, 
+            { text: this.state.newCommentText}
+           ]
+        });
+
+        this.setState({ newCommentText: '' })
+
+    }
+
+    handleTextChange(e) {
+        this.setState({ newCommentText: e.target.value });
+    }
+
+    render() {
+        return (
+            <div>
+                <h2>{ this.props.title }</h2>
+
+                <form onSubmit={ this.handleSubmit }>
+                    <input 
+                        value= { this.state.newCommentText } 
+                        onChange={ this.handleTextChange } />
+                    <button type="submit">Comment</button>
+                </form>
+
+                { this.state.comments.map((comment, index) => {
+                        return <Comment key={ index } commentText={ comment.text } />
+                    }) }
+            </div>
+        )
+    }
+}
